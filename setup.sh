b079ba43f7b940d14c8e8fe33336f5cb763394e8
aayush-1306 <aayush@profjim.com>
2024-12-23 00:01:14 +0530

    .

--- setup.sh
+++ setup.sh
@@ -2,3 +2,7 @@ pip3 install /SyncTalk/freqencoder
 pip3 install /SyncTalk/shencoder
 pip3 install /SyncTalk/gridencoder
 pip3 install /SyncTalk/raymarching
+
+python3 sanity_check.py
+cd data_utils/face_tracking && python3 convert_BFM.py
+cd /SyncTalk
